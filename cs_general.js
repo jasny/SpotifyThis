3ac223f526f404e188dfb9d5cc71964fd8f62a0d
Arnold Daniels <arnold@jasny.net>
2012-12-02 14:26:32 +0100

    Don't show popup for last.fm

--- cs_general.js
+++ cs_general.js
@@ -1,12 +1,14 @@
-var type, title;
-var meta = document.getElementsByTagName('meta');
+if (!window.location.href.match(/^https?:\/\/\w+.(wikipedia\.org|last\.fm)\//)) {
+  var type, title;
+  var meta = document.getElementsByTagName('meta');
 
-var preview = document.getElementsByClassName('track-preview')[0];
-if (preview) preview.style.display = 'none';
+  var preview = document.getElementsByClassName('track-preview')[0];
+  if (preview) preview.style.display = 'none';
 
-for (var i=0; i < meta.length; i++) {
-  if (meta[i].getAttribute('property') == 'og:type') type = meta[i].getAttribute('content');
-    else if (meta[i].getAttribute('property') == 'og:title') title = meta[i].getAttribute('content')
+  for (var i=0; i < meta.length; i++) {
+    if (meta[i].getAttribute('property') == 'og:type') type = meta[i].getAttribute('content');
+      else if (meta[i].getAttribute('property') == 'og:title') title = meta[i].getAttribute('content')
+  }
+
+  chrome.extension.sendMessage({ 'site' : '*', 'type': type, 'title': title });
 }
-
-chrome.extension.sendMessage({ 'site' : '*', 'type': type, 'title': title });
